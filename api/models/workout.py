300fbeff39a8b50eb32d5683f5f00d744c1a9e77
perryfhuang <perryfhuang@gmail.com>
2020-09-21 10:52:41 -0400

    Remove updated_at field for workouts
    Causing weird order of workouts -> renders in
    random order on Feed
    
    Testing removing updated_at field to see if it keeps workouts in
    chornological order even after editing

--- api/models/workout.py
+++ api/models/workout.py
@@ -67,7 +67,6 @@ class Workout(models.Model):
   feeling = models.CharField(max_length=100, blank=True, null=True)
 
   created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
   owner = models.ForeignKey(
       get_user_model(),
       on_delete=models.CASCADE
